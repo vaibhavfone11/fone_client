70e4496c9696d1d7d2ef4a1eaae66c1cd469b141
Vaibhav Vekariya <vaibhavvekariya58@gmail.com>
2025-03-24 20:59:17 -0400

    vendor added

---
+++ src/app/components/vendor/vendor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendor',
+  imports: [],
+  templateUrl: './vendor.component.html',
+  styleUrl: './vendor.component.scss'
+})
+export class VendorComponent {
+
+}
